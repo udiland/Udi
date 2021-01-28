75bf9c0125b4a80b0def5d541c1bd3d7d0fc2b51
udiland <udiland@tauex.tau.ac.il>
2021-01-28 15:04:00 +0200

    update

--- my_filter/get_stat.py
+++ my_filter/get_stat.py
@@ -34,9 +34,11 @@ def get_fam_data(path_to_folder):
             df.loc[0, "No. of genes in family"] = n_genes
 
             # get the result of the filtering
-
-            tbl = pd.read_csv(path_to_folder + "/" + f + "/output_filtered_final.csv", na_values="")
-
+            try:
+                tbl = pd.read_csv(path_to_folder + "/" + f + "/output_filtered_final.csv", na_values="")
+            except: # If there is no output table
+                print("No filtering output for family " + f)
+                continue
 
             # check if there are no sgrna
             if tbl.iloc[0:1,:].isnull().values.all():
